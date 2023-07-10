55692af1ecbe76e269e1e2dda605a8dc9309be8e
mar-vyn23 <marvovybes23@gmail.com>
2023-07-10 13:17:19 +0300

    Code has been modified to fetch rows from the database in batches of 100 at a time , convert them to a geojson object and store the geojson objects in a list , and then plot the choropleth map , the trick has been to render the template on the default route and then in the maps.html file to call the route that returns the geojson object , i'm still improving the code

--- app.py
+++ app.py
@@ -1,66 +1,65 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 import psycopg2
-import geojson
 import os
 from dotenv import load_dotenv
 import shapely.wkb as wkb
 import shapely.geometry as geometry
+import geojson
+import time
 
 app = Flask(__name__)
-
-# load the .env file
 load_dotenv()
-
-db_config = {
-    'host': os.environ.get('DB_HOST'),
-    'port': os.environ.get('DB_PORT'),
-    'database': os.environ.get('DB_NAME'),
-    'user': os.environ.get('DB_USER'),
-    'password': os.environ.get('DB_PASSWORD'),
-}
-
-try:
-    connection = psycopg2.connect(**db_config)
-except psycopg2.Error as connection_error:
-    print('Unable to establish connection to the database', connection_error)
-
-else:
-    # Create a cursor object -> session to the database
-    cursor = connection.cursor()
-    # Retrieve 3 columns from the database
-    cursor.execute('SELECT parish,pm2_5,geometry FROM public."Prediction"')
-    # Return all rows of the 3 columns above
-    data = cursor.fetchall()
-    # close connection after fetching data from db
-    cursor.close()
-
-
-# Transform the data into a geojson
-def transform_to_geojson():
-    features = []
-    for row in data:
-        # convert the geometry from hex to bytes
-        wkb_geometry = bytes.fromhex(row[2])
-        # convert the wkb geometry to a shapely object
-        geometry_object = wkb.loads(wkb_geometry)
-        # now convert the geometry to a geojson
-        geometry_geojson = geometry.mapping(geometry_object)
-        properties = {
-            'parish': row[0],
-            'pm2_5': row[1]
-        }
-        feature = geojson.Feature(geometry=geometry_geojson, properties=properties)
-        features.append(feature)
-    feature_collection = geojson.FeatureCollection(features)
-    json_dump = geojson.dumps(feature_collection)
-    return json_dump
+app.template_folder = 'templates'
 
 
 @app.route('/')
-def postgres_connection():
-    results = transform_to_geojson()
-    return render_template('map.html', results=results)
+def home():
+    return render_template('map.html')
 
 
-app.run(debug=False)
+@app.route('/geo')
+def connect_to_db():
+    db_config = {
+        'host': os.environ.get('DB_HOST'),
+        'port': os.environ.get('DB_PORT'),
+        'database': os.environ.get('DB_NAME'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+    }
+
+    try:
+        connection = psycopg2.connect(**db_config)
+    except psycopg2.Error as e:
+        print(f'Error establishing connection to db : {e}')
+    else:
+        limit = 100
+        offset = 0
+        data = []
+        while True:
+            # get first 100 records
+            cursor = connection.cursor()
+            cursor.execute('SELECT parish,pm2_5,geometry FROM public."Prediction" OFFSET %s LIMIT %s', (offset, limit))
+            rows = cursor.fetchall()
+            for row in rows:
+                wkb_geometry = bytes.fromhex(row[2])
+                geometry_object = wkb.loads(wkb_geometry)
+                geometry_geojson = geometry.mapping(geometry_object)
+                properties = {
+                    'parish': row[0],
+                    'pm2_5': row[1]
+                }
+                feature = geojson.Feature(geometry=geometry_geojson, properties=properties)
+                geo_json = geojson.dumps(feature)
+                # print(geo_json)
+                # print("\n\n")
+                data.append(geo_json)
+                time.sleep(10)
+            offset += limit
+            if len(rows) < limit:
+                break
+        return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
