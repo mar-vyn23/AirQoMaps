383c9852ab75afab543439eae1b69880fc01af97
mar-vyn23 <marvovybes23@gmail.com>
2023-07-13 15:56:52 +0300

    Maps work , but they load slowly , still need optimization

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import os
+import time
+
 import geojson
 import psycopg2
 import shapely.geometry as geometry
 import shapely.wkb as wkb
 from dotenv import load_dotenv
-from flask import Flask, jsonify
-from flask import render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 load_dotenv()
 
 
-@app.route('/')
-def home():
-    return render_template('map.html')
-
-
-@app.route('/geo')
 def connect_to_db():
     db_config = {
         'host': os.environ.get('DB_HOST'),
@@ -32,12 +27,10 @@ def connect_to_db():
         print(f'Error establishing connection to db : {e}')
     else:
         cursor = connection.cursor()
-        cursor.execute('SELECT parish,pm2_5,geometry FROM public."Prediction"')
-        rows = cursor.fetchall()
-        features = []
+        cursor.execute('SELECT parish, pm2_5, geometry FROM public."Prediction"')
         i = 0
-        for row in rows:
-            if i < 16863:
+        for row in cursor:
+            if i < 10:
                 wkb_geometry = bytes.fromhex(row[2])
                 geometry_object = wkb.loads(wkb_geometry)
                 geometry_geojson = geometry.mapping(geometry_object)
@@ -45,14 +38,26 @@ def connect_to_db():
                     'parish': row[0],
                     'pm2_5': round(row[1], 2)
                 }
-                i = i + 1
-                feature = geojson.Feature(geometry=geometry_geojson,
-                                          properties=properties)
-                features.append(feature)
-        feature_collection = geojson.FeatureCollection(features)
+                i += 1
+                feature = geojson.Feature(geometry=geometry_geojson, properties=properties)
+                yield feature
+                print(feature)
+                time.sleep(4)
         cursor.close()
         connection.close()
-        return jsonify(feature_collection)
+
+
+@app.route('/')
+def home():
+    return render_template('map.html')
+
+
+@app.route('/geo')
+def get_geo_data():
+    feature_generator = connect_to_db()
+    feature_list = list(feature_generator)
+    feature_collection = geojson.FeatureCollection(feature_list)
+    return jsonify(feature_collection)
 
 
 if __name__ == "__main__":
