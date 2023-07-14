67290862dfd7e0727c6586b6bcb03bdcdfc9f26e
mar-vyn23 <marvovybes23@gmail.com>
2023-07-14 11:29:31 +0300

    Improved code quality

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import os
-import time
-
 import geojson
 import psycopg2
 import shapely.geometry as geometry
 import shapely.wkb as wkb
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template
+from flask_caching import Cache
 
 app = Flask(__name__)
 load_dotenv()
 
+app.config['CACHE_TYPE'] = 'redis'
+app.config['CACHE_REDIS_HOST'] = 'localhost'
+app.config['CACHE_REDIS_PORT'] = 6379
+app.config['CACHE_REDIS_URL'] = 'redis://localhost:6379/0'
+cache = Cache(app=app)
 
+
+@cache.cached(timeout=3600, key_prefix='geo_data')
 def connect_to_db():
     db_config = {
         'host': os.environ.get('DB_HOST'),
@@ -30,7 +36,7 @@ def connect_to_db():
         cursor.execute('SELECT parish, pm2_5, geometry FROM public."Prediction"')
         i = 0
         for row in cursor:
-            if i < 10:
+            if i < 16863:
                 wkb_geometry = bytes.fromhex(row[2])
                 geometry_object = wkb.loads(wkb_geometry)
                 geometry_geojson = geometry.mapping(geometry_object)
@@ -41,8 +47,6 @@ def connect_to_db():
                 i += 1
                 feature = geojson.Feature(geometry=geometry_geojson, properties=properties)
                 yield feature
-                print(feature)
-                time.sleep(4)
         cursor.close()
         connection.close()
 
