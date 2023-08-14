979aa531192181542c092b460ca6051562c420db
mar-vyn23 <marvovybes23@gmail.com>
2023-08-14 18:39:58 +0300

    Loading nearby parishes from the current location progressively

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import geojson
 import psycopg2
 import shapely.wkb as wkb
 from dotenv import load_dotenv
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_caching import Cache
 
 # Create a Flask app
@@ -47,30 +47,39 @@ def connect_to_db():
         print(f'Error establishing connection to db : {e}')
     else:
         global offset
-        chunk_size = 50
+        # print(f'Offset is now {offset} instead of 0')
+        chunk_size = 100
+        offset = 0
+        # print(f'Offset has now been set to {offset}')
         cursor = connection.cursor()
-        cursor.execute('SELECT COUNT(*) FROM public."Prediction"')
-        total_rows = cursor.fetchone()[0]
 
-        # Check if there are more rows to fetch based on the current offset
-        if offset < total_rows:
-            # Fetch a chunk of records from the database using LIMIT and OFFSET clauses
-            cursor.execute(f'SELECT parish, pm2_5, geometry FROM public."Prediction" LIMIT {chunk_size} OFFSET {offset}')
-            rows = cursor.fetchall()
+        # Get user's latitude and longitude from query parameters
+        user_latitude = float(request.args.get('latitude'))
+        user_longitude = float(request.args.get('longitude'))
 
-            # Convert each row to GeoJSON feature using the 'to_geojson' function and build a FeatureCollection
-            features = [to_geojson(row[0], round(row[1]), row[2], id=i + offset) for i, row in enumerate(rows)]
-            feature_collection = geojson.FeatureCollection(features)
+        # Get the bounds of the visible map area (adjust buffer as needed)
+        buffer = 1  # Adjust this value as needed for the buffer
+        min_lat = user_latitude - buffer
+        max_lat = user_latitude + buffer
+        min_lng = user_longitude - buffer
+        max_lng = user_longitude + buffer
 
-            # Increment the offset for the next chunk
-            offset = offset + chunk_size
+        # Fetch parishes within the visible map area from the database
+        cursor.execute(f'SELECT parish, pm2_5, geometry FROM public."Prediction" WHERE ST_Within(geometry, ST_MakeEnvelope(%s, %s, %s, %s, 4326)) LIMIT {chunk_size} OFFSET {offset}',
+                       (min_lng, min_lat, max_lng, max_lat))
+        rows = cursor.fetchall()
+        # print(f"rows for {min_lat}, {max_lat} fetched")
 
-            # Wait for 1 second before fetching another batch
-            time.sleep(1)
-        else:
-            # If no more rows to fetch, return an empty FeatureCollection
-            feature_collection = geojson.FeatureCollection([])
+        # Convert each row to GeoJSON feature using the 'to_geojson' function and build a FeatureCollection
+        features = [to_geojson(row[0], round(row[1]), row[2], id=i + offset) for i, row in enumerate(rows)]
+        feature_collection = geojson.FeatureCollection(features)
+        # print(f"feature collection for {min_lat}, {max_lat}, {min_lng}, {max_lng} are {len(feature_collection)}")
 
+        # Increment the offset for the next chunk
+        # offset = offset + chunk_size
+
+        # Wait for 1 second before fetching another batch
+        time.sleep(1)
         cursor.close()
         connection.close()
         return jsonify(feature_collection)  # Return the GeoJSON data as a JSON response
